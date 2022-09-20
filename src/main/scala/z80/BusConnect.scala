1310a5efc226d8eec6c37c950749960dda10ba95
nanitsuku <nanitsuku@outlook.jp>
2022-09-20 12:20:22 +0900

    fix error when synthesize

--- src/main/scala/z80/BusConnect.scala
+++ src/main/scala/z80/BusConnect.scala
@@ -30,7 +30,7 @@ class BusConnect(size:Int = 2, width:Int = 8) extends BlackBox with HasBlackBoxI
     |${lists.map(n => f"\tinput ${name_dirs_(n)}").mkString(",\n")}
     |);
     |
-    |${lists.map(a => f"wire [${ind}:0] output_${a}_ = " + (lists.withFilter(b => b!=a).map(n => f"(${name_dirs_(n)}?${name_input(n)}:${width}'Z)").mkString("|"))).mkString(";\n")};
+    |${lists.map(a => f"wire [${ind}:0] output_${a}_ = " + (lists.withFilter(b => b!=a).map(n => f"(${name_dirs_(n)}?${name_input(n)}:${width}'bZ)").mkString("|"))).mkString(";\n")};
     |
     |${lists.map(n => f"assign ${name_output_(n)} = output_${n}_").mkString(";\n")};
     |
